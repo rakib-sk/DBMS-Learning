42d5410888794990b8849122a909d129990a6e2e
Rakib Hoseen <rh01828315879@gmail.com>
2025-11-11 19:13:41 +0600

    Create ceo.sql

---
+++ Class3/ceo.sql
@@ -0,0 +1,8 @@
+INSERT INTO ceo (id, name, email, phone)
+VALUES
+(1, "Fahim vai", "abc@gmail.com", "017xxxxxx"),
+(2, "Zahid vai", "xyz@gmail.com", "017abcd"),
+(3, "Zehad", "pqr@gmail.com", "018pqr"),
+(4, "Rakib", "mno@gmail.com", "018mno"),
+(5, "Sanjida apu", "ijk@gmail.com", "018ijk"),
+(6, "Lamia apu", "sqr@gmail.com", "018sdk");
